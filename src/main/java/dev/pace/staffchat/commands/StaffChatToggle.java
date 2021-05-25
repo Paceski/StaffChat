020ad32225015ac731cd1bea543b876592737db5
Pace <pacex3123@gmail.com>
2021-05-25 21:00:31 +0200

    monkey

--- src/main/java/dev/pace/staffchat/commands/StaffChatToggle.java
+++ src/main/java/dev/pace/staffchat/commands/StaffChatToggle.java
@@ -5,7 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class StaffChatToggle implements CommandExecutor {
 
@@ -25,7 +24,7 @@ public class StaffChatToggle implements CommandExecutor {
 
         boolean isEnabled = Main.getInstance().toggledSC.get(player.getUniqueId());
         boolean previousValue = Main.getInstance().toggledSC.put(player.getUniqueId(), !isEnabled);
-        player.sendMessage(previousValue ? Main.config.getString("staffchat.toggle-on"): Main.config.getString("staffchat.toggle-off"));
+        player.sendMessage(!previousValue ? Main.config.getString("staffchat.toggle-on"): Main.config.getString("staffchat.toggle-off"));
         return true;
     }
 }
